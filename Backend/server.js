6e2a85d363aeedcb3aceb75b9e248b75610bf02a
smarika <mesmarika7@gmail.com>
2026-02-23 20:19:03 +0545

    fixed all changes made before

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,32 @@
-import express from "express";
+import express from "express"
 import { connection } from "./src/Database/db.js";
-import authRoutes from "./src/Routes/authRoutes.js";
+import authRoutes from "./src/Routes/authRoutes.js"
 import "./src/Config/passport.js";
-import cors from "cors";
+import cors from "cors"
 import passport from "passport";
 import { protect } from "./src/Middleware/authmiddleware.js";
-import "./src/Model/index.js";
-import adminRoutes from "./src/Routes/adminRoutes.js";
-import userRoutes from "./src/Routes/userRoutes.js";
-import dotenv from "dotenv";
-dotenv.config();
+import "./src/Model/index.js"
+import adminRoutes from "./src/Routes/adminRoutes.js"
+import userRoutes from "./src/Routes/userRoutes.js"
+
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port="3000"
 
-connection();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-);
+connection()
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true,
+}))
 
-app.use("/uploads", express.static("uploads"));
-app.use(passport.initialize());
-app.use(express.json());
-app.use("/auth", authRoutes);
-app.use(protect);
-app.use("/user", userRoutes);
-app.use("/admin", adminRoutes);
-app.listen(port, () => {
-  console.log(`server running in port ${port}`);
-});
+app.use('/uploads', express.static('uploads'));
+app.use(passport.initialize()); 
+app.use(express.json())
+app.use("/auth",authRoutes)
+app.use(protect)
+app.use("/user",userRoutes);
+app.use("/admin",adminRoutes)
+app.listen(port,()=>{
+    console.log(`server running in port ${port}`)
+})
--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-    baseURL:API_URL,
+    baseURL:"http://localhost:3000",
     withCredentials:false
 })
 api.interceptors.request.use((config) => {
